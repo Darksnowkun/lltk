7f069d65ac160afa65d1e36458aec8dfe810cf66
Brendan Tierney <brendantierney.thanh@gmail.com>
2022-06-05 03:37:21 +0000

    Bootstrap is now loaded and ready to use. Templates folder is now ready to use. Initial Index file created.

--- myproject/core/settings.py
+++ myproject/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'pages',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -91,6 +94,11 @@ DATABASES = {
         'PORT': '5432', 
     }
 }
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'var/www/static/',
+]
 
 
 # Password validation
